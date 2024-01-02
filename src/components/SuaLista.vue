08fe72ee4385b12ffd53d454cbdc3148d2444a78
lucialhev <luciahevicente@gmail.com>
2024-01-02 08:25:43 -0300

    alteração SuaLista

---
+++ src/components/SuaLista.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export default{
+        data(){
+            return{
+                ingredientes: [] 
+            }
+        }
+    }
+</script>
+<template>
+    <section>
+            <span class="subtitulo-lg sua-lista-texto">
+                Sua lista:
+            </span>
+            <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+                <li v-for="ingrediente in ingredientes" :key="ingrediente">
+                    <Tag :texto="ingrediente" ativa/>
+                </li>
+            </ul>
+            <p v-else class="paragrafo lista-vazia">
+              <img src="../assets/images/icones/lista-vazia.svg" alt="icone de pesquisa">
+              Sua lista está vazia, selecione ingredientes para iniciar.
+            </p>
+        </section>
+</template>
